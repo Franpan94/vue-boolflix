661a3ee34bfbc2028ab028cae06d5c9185b424e1
Franpan94 <francescopanico94@libero.it>
2022-07-21 15:17:21 +0200

    creation of structures

---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <FilmList />
+    main
+  </div>
+</template>
+
+<script>
+import FilmList from './FilmList.vue'
+
+export default {
+   name: 'FilmList',
+   components: {
+    FilmList
+   }
+}
+</script>
+
+<style>
+
+</style>
